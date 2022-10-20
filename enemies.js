43ddcae80e9e37decdb84933744c780da9eae1fe
Federico Barbieri <federicobarbieri54@gmail.com>
2022-10-20 18:59:44 +0200

    create logic for bats wing movement

--- enemies.js
+++ enemies.js
@@ -5,30 +5,55 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
+const numberOfEnemies = 10;
+
+const enemiesArray = [];
+
+
+let gameFrame = 0;
+
 class Enemy {
     constructor(){
-        this.x = 10;
-        this.y = 50;
-        this.width = 100;
-        this.height = 100;
+        this.image = new Image();
+        this.image.src = 'img/enemy1.png';
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update(){
-        this.x++;
-        this.y++;
+        this.x+= this.speed;
+        this.y+= this.speed;
+        // animate sprites
+        if (gameFrame % this.flapSpeed === 0){
+        this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
     }
     draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,
+            this.x, this.y, this.width, this.height);
     }
 }
 
-const enemy1 = new Enemy();
-
+for (let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy());
+}
 
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.update();
-    enemy1.draw();
+
+   enemiesArray.forEach(enemy => {
+    enemy.update();
+    enemy.draw();
+   });
+   gameFrame++;
     requestAnimationFrame(animate);
 }
 
